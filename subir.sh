efc011416d26520feeaf03f1334110333a91dcee
pitist <pitist@example.com>
2025-07-24 22:24:53 +0200

    Subida automática desde Termux

--- subir.sh
+++ subir.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-GITHUB_USER="pitist"
-REPO_NAME="backup_musica"
+USER="pitist"
+REPO="backup_musica"
 TOKEN="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"
 
 cd ~/Music_backup || exit 1
 
 [ -d .git ] || git init
-git config user.name "pitist"
+git config user.name "$USER"
 git config user.email "pitist@example.com"
 git remote remove origin 2>/dev/null
 
-curl -s -H "Authorization: token $TOKEN"      -d "{\"name\":\"$REPO_NAME\"}"      https://api.github.com/user/repos > /dev/null
+curl -s -H "Authorization: token $TOKEN"      -d "{\"name\":\"$REPO\"}"      https://api.github.com/user/repos
 
-git remote add origin https://$TOKEN@github.com/$GITHUB_USER/$REPO_NAME.git
+git remote add origin https://$TOKEN@github.com/$USER/$REPO.git
 
 git add .
 git commit -m "Subida automática desde Termux" 2>/dev/null || echo "Nada nuevo que commitear"
